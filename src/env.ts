8f5aea4f9c3352876bd7edd9209bbd05093bf600
rahul <nits.rahul.gupta@gmail.com>
2023-10-02 17:10:59 +0530

    first commit

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,8 @@
-const config =
-  process.env.NODE_ENV !== "production" ? await import("dotenv") : null
+import { config } from "dotenv"
 
-if (config) config.config()
+// loading specific env files
+// eg. env.production or env.development
+// change your as per your requirement
+config({ path: `.env.${process.env.NODE_ENV}` })
 
 export const PORT = process.env.PORT || 5000
